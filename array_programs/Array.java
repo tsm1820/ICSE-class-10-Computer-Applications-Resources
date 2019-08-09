a45a36871ecc2115ff107fc8a609267926d6da6a
tsm1820 <53284424+tsm1820@users.noreply.github.com>
2019-08-09 11:48:50 +0530

    Update Array.java

--- array_programs/Array.java
+++ array_programs/Array.java
@@ -1,19 +1,23 @@
+/* 
+SIMPLE ARRAY PROGRAM TO TAKE INPUT AND PRINT ALL 
+ELEMENTS OF THE ARRAY
+*/
 import java.util.Scanner;//PRINT ALL ELEMENTS
 class test
 {
 	public static void main(String args[])
 	{
 		Scanner sc=new Scanner(System.in);
-		int id[]=new int[10];
-		for(int i=0;i<id.length;i++)
+		int id[]=new int[10]; //CREATES ARRAY OF SIZE 10
+		for(int i=0;i<id.length;i++) //TRAVERSES THE ENTIRE LENGTH OF THE ARRAY TO TAKE INPUT
 		{
 			System.out.println("Enter "+i+" element");
 			id [i]=sc.nextInt();
 		}
-		//print loop
+		//PRINT LOOP
 		for(int i=0;i<id.length;i++)
 		{
-			System.out.println("\t"+i+"->"+id[i]);//array is always represented by id[i]
+			System.out.println("\t"+i+"->"+id[i]);//Array is always represented by id[i]
 		}
 	}
 }
